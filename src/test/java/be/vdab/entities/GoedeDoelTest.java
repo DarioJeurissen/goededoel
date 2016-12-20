6af662b0f7ae9511c80da30019ac4e45f85bc8c9
Dario.Jeurissen <d.jeurissen@gmail.com>
2016-12-20 15:09:54 +0100

    extra

--- src/test/java/be/vdab/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/entities/GoedeDoelTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
